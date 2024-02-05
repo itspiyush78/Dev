62450114c0cdf98dd4cb5b979b8792b1c942e038
itspiyush78 <itspiyush78@gmail.com>
2024-02-05 22:27:47 +0530

    equality operator

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,11 +1,6 @@
-﻿int[] inventory = {200, 450, 700, 175, 250};
-int sum = 0;
-int bin = 0;
-foreach (int items in inventory)
-{
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-}
+﻿Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
 
-Console.WriteLine($"We have {sum} items in inventory.");
+string myValue = "a";
+Console.WriteLine(myValue == "a");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f9782651429d210f6b654a881cd0be28147ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24cbd754202483d65e2b226d9fc4ef38fedffe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
