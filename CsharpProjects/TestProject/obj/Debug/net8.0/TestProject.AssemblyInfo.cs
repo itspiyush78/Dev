cb35a5b1f469df4b06a75bf66aa6cb1d5b93d9df
itspiyush78 <itspiyush78@gmail.com>
2024-02-06 21:46:41 +0530

    types of operator done

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24cbd754202483d65e2b226d9fc4ef38fedffe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62450114c0cdf98dd4cb5b979b8792b1c942e038")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
