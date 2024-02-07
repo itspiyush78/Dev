e5bdac14d2a1c7ce8f409080ad6e204951f2fd1a
itspiyush78 <itspiyush78@gmail.com>
2024-02-07 23:23:14 +0530

    variable

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,48 +1,9 @@
-﻿// equality operator
-
-// Console.WriteLine("a" == "a");
-// Console.WriteLine("a" == "A");
-// Console.WriteLine(1 == 2);
-
-// string myValue = "a";
-// Console.WriteLine(myValue == "a");
-
-// inequality operator
-
-// Console.WriteLine("a" != "a");
-// Console.WriteLine("a" != "A");
-// Console.WriteLine(1 != 2);
-
-// string myValue = "a";
-// Console.WriteLine(myValue != "a");
-
-// Comparison operator
-
-// Console.WriteLine(1 > 2);
-// Console.WriteLine(1 < 2);
-// Console.WriteLine(1 >= 1);
-// Console.WriteLine(1 <= 1);
-
-//  use method that returns a Boolean
-
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(pangram.Contains("fox"));
-// Console.WriteLine(pangram.Contains("cow"));
-
-// logical negation
-
-// These two lines of code will create the same output
-
-// Console.WriteLine(pangram.Contains("fox") == false);
-// Console.WriteLine(!pangram.Contains("fox"));
-
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(!pangram.Contains("fox"));
-// Console.WriteLine(!pangram.Contains("cow"));
-
-// conditional operator
-
-int saleAmount = 1001;
-//int discount = saleAmount > 1000 ? 100 : 50;
-// Console.WriteLine($"Discount: {discount}");
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+﻿//  
+bool flag = true;
+int value = 0;
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62450114c0cdf98dd4cb5b979b8792b1c942e038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb35a5b1f469df4b06a75bf66aa6cb1d5b93d9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
