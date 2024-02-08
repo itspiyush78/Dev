e2463393f47f920fb73bb83e9e9e5ca6cb602370
itspiyush78 <itspiyush78@gmail.com>
2024-02-08 22:43:38 +0530

    code block

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb35a5b1f469df4b06a75bf66aa6cb1d5b93d9df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bdac14d2a1c7ce8f409080ad6e204951f2fd1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
