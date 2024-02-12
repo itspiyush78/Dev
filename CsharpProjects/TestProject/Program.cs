33da3c8925ca2b5f73942fd144b4fd3548c8a445
itspiyush78 <itspiyush78@gmail.com>
2024-02-12 08:52:04 +0530

    for each done

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -9,8 +9,36 @@
 // Console.WriteLine($"Outside the code block: {value}");
 
 
-bool flag = true;
-if (flag)
-{
-    Console.WriteLine(flag);
-}
+// bool flag = true;
+// if (flag) Console.WriteLine(flag);
+
+// larger if-elseif-else//
+
+// string name = "steve";
+// if (name == "bob") Console.WriteLine("Found Bob");
+// else if (name == "steve") Console.WriteLine("Found Steve");
+// else Console.WriteLine("Found Chuck");
+
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+// foreach (int number in numbers)
+// {
+//     int total;
+
+//     total += number;
+
+//     if (number == 42)
+//     {
+//        bool found = true;
+
+//     }
+
+// }
+
+// if (found) 
+// {
+//     Console.WriteLine("Set contains 42");
+
+// }
+
+// Console.WriteLine($"Total: {total}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bdac14d2a1c7ce8f409080ad6e204951f2fd1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2463393f47f920fb73bb83e9e9e5ca6cb602370")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
