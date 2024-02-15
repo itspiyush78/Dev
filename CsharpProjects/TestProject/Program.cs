ca8f05b9d6637c90207a88935d5669fb7cc9e89b
itspiyush78 <itspiyush78@gmail.com>
2024-02-15 11:34:59 +0530

    deep dive in do-while loop

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -35,11 +35,29 @@
 
 /* Do-while statement*/
 
-Random random = new Random();
-int current = 0;
+// Random random = new Random();
+// int current = 0;
 
+// do
+// {
+//     current = random.Next(1, 11);
+//     Console.WriteLine(current);
+// } while (current != 7);
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+/*
 do
 {
     current = random.Next(1, 11);
     Console.WriteLine(current);
 } while (current != 7);
+*/
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
